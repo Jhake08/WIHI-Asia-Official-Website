5828be63912dcb1d0f8a7f89564df75bd5c17ecb
AdminJhake <112927916+Jhake08@users.noreply.github.com>
2025-07-20 10:48:53 +0000

    Final commit before completion

--- app/about/CompanyTimelineModal.tsx
+++ app/about/CompanyTimelineModal.tsx
@@ -52,7 +52,6 @@ export default function CompanyTimelineModal({ isOpen, onClose }: CompanyTimelin
           <li>The company began as a part-time venture, driven by CEO Mr. Jan Lester Julian and COO Mrs. Jellete Julian, with the goal of building a better future for their family through stable income and opportunity. 
           As the owners of the company, they achieved a major breakthrough in 2023 with the launch of Cosmi Beautii
           which became a top TikTok brand—reaching 20,000 daily orders for its niacinamide soap and continuing to grow ever since.</li>
-
         </ul>
       </div>
       <style jsx>{`
